e46b20e9a80e9258c2d28f2a93e379c8104b1cf5
cl2522 <chaelin@uchicago.edu>
2025-05-06 16:47:16 +0000

    done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,4 +1,60 @@
 class DirectorsController < ApplicationController
+  def update
+    # Get the ID out of params
+    d_id = params.fetch("the_id")
+
+    # Look up the existing record
+    matching_records = Director.where({ :id => d_id })
+    the_director = matching_records.at(0)
+
+    # Overwrite each column with the values from user inputs
+    the_director.name = params.fetch("name")
+    the_director.dob = params.fetch("dob")
+    the_director.bio = params.fetch("bio")
+    the_director.image = params.fetch("the_image")
+
+    # Save
+
+    the_director.save
+
+    # Redirect to the movie details page
+    redirect_to("/directors/#{the_director.id}")
+  end
+  
+  def create
+    # params hash looks like this:
+    # {"the_title"=>"1", "the_year"=>"2", "the_duration"=>"3", "the_description"=>"4", "the_image"=>"5", "the_director_id"=>"6"}
+
+    d = Director.new
+    d.name = params.fetch("name")
+    d.dob = params.fetch("dob")
+    d.bio = params.fetch("bio")
+    d.image = params.fetch("the_image")
+
+    d.save
+
+    redirect_to("/directors")
+
+    # Retrieve the user's inputs from params
+    # Create a record in the movie table
+    # Populate each column with the user input
+    # Save
+
+    # Redirect the user back to the /movies URL
+  end
+  
+  def destroy
+    the_id = params.fetch("an_id")
+
+    matching_records = Director.where({ :id => the_id })
+
+    the_director = matching_records.at(0)
+
+    the_director.destroy
+
+    redirect_to("/directors")
+  end
+  
   def index
     matching_directors = Director.all
     @list_of_directors = matching_directors.order({ :created_at => :desc })
